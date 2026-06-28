60699ad287580e5cd8650204d4415da10346d060
applyuser160 <syunsuke130121@gamil.com>
2026-06-28 16:30:59 +0900

    サンプルファイル追加

---
+++ src/template/sample.py
@@ -0,0 +1,11 @@
+def add(a: int, b: int) -> int:
+    """Add two integers.
+
+    Args:
+        a: First integer.
+        b: Second integer.
+
+    Returns:
+        The sum of the two integers.
+    """
+    return a + b
---
+++ tests/test_sample.py
@@ -0,0 +1,6 @@
+from template.sample import add
+
+
+def test_add() -> None:
+    assert add(1, 2) == 3
+    assert add(-1, 1) == 0
